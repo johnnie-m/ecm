734a3f6b0ffa1828797d775d71ba2e2102cfa9f1
johnnie-m <93076135+johnnie-m@users.noreply.github.com>
2024-05-16 10:46:04 +0300

    Update values_substitute.groovy

--- values_substitute.groovy
+++ values_substitute.groovy
@@ -20,17 +20,18 @@ pipeline {
                 withCredentials([usernamePassword(credentialsId: 'vault-2', passwordVariable: 'vltPwd', usernameVariable: 'vltUsr')]) {
                     script {
                         sh '''#!/bin/bash
-                        
+                        # Fetch vault client token:
                         JSON=$( jq -n \
                             --arg password $vltPwd \
                             '$ARGS.named')
-
                         C_TOKEN=$(curl "${vltUrl}/v1/auth/userpass/login/${vltUsr}" --data "$JSON" 2</dev/null | jq -r '.auth.client_token')
-
+                        
+                        # Fetch secrets & export to env variables
                         export AMQP_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.AMQP_URL')
                         export ELMA365_AMQP_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_AMQP_URL')
                         export AMQP_URL2='asdfgdfsgdf'
-                        
+
+                        # Substitute secrets to values.yaml file
                         envsubst < values.yml > values1.yml
                         echo "showing values1.yml"
                         echo "------------------------------------------------------"
