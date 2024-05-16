463119461e180d625e82627f6cdf2e68121dae04
johnnie-m <93076135+johnnie-m@users.noreply.github.com>
2024-05-16 10:30:35 +0300

    Create values_substitute.groovy

---
+++ values_substitute.groovy
@@ -0,0 +1,46 @@
+pipeline {
+
+    agent {label 'jnk-02.test'
+    }
+
+    environment {
+    vltUrl='192.168.0.50:8200'
+    }
+
+    stages {
+        
+        stage ('Clone git repo') {
+            steps {
+                git branch: 'main', url: 'https://github.com/johnnie-m/ecm.git', credentialsId: 'jm_git'    
+            }    
+        }
+        
+        stage ('Fetch secrets') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'vault-2', passwordVariable: 'vltPwd', usernameVariable: 'vltUsr')]) {
+                    script {
+                        sh '''#!/bin/bash
+                        
+                        JSON=$( jq -n \
+                            --arg password $vltPwd \
+                            '$ARGS.named')
+
+                        C_TOKEN=$(curl "${vltUrl}/v1/auth/userpass/login/${vltUsr}" --data "$JSON" 2</dev/null | jq -r '.auth.client_token')
+
+                        export AMQP_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.AMQP_URL')
+                        export ELMA365_AMQP_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_AMQP_URL')
+                        export AMQP_URL2='asdfgdfsgdf'
+                        
+                        envsubst < values.yml > values1.yml
+                        echo "showing values1.yml"
+                        echo "------------------------------------------------------"
+                        cat values1.yml
+                        echo "------------------------------------------------------"
+                        
+                        '''
+                    }
+                }
+            }
+        }
+    }    
+}
