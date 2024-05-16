dbc66124e47c44c16b3761fc2a26ff48bf5270dc
johnnie-m <93076135+johnnie-m@users.noreply.github.com>
2024-05-16 14:50:12 +0300

    Update values_substitute.groovy

--- values_substitute.groovy
+++ values_substitute.groovy
@@ -25,8 +25,10 @@ pipeline {
                             --arg password $vltPwd \
                             '$ARGS.named')
 
+                        # Fetch vault user token
                         C_TOKEN=$(curl "${vltUrl}/v1/auth/userpass/login/${vltUsr}" --data "$JSON" 2</dev/null | jq -r '.auth.client_token')
 
+                        # Fetch secrets and export to environment variables
                         export AMQP_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.AMQP_URL')
                         export ELMA365_AMQP_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_AMQP_URL')
                         export ELMA365_MONGO_DB_NAME=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_MONGO_DB_NAME')
@@ -50,11 +52,22 @@ pipeline {
                         export S3_SSL_ENABLED=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.S3_SSL_ENABLED')
                         export S3_UPLOAD_METHOD=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.S3_UPLOAD_METHOD')
                         export VAHTER_MONGO_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.VAHTER_MONGO_URL')
+                        export HOSTNAME=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.HOSTNAME')
+                        export IMG_REP=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.IMG_REP')
+                        export SU_EMAIL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SU_EMAIL')
+                        export SU_PASS=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SU_PASS')
+                        export S3_BUCKET=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.S3_BUCKET')
+                        export SMTP_HOST=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SMTP_HOST')
+                        export SMTP_PORT=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SMTP_PORT')
+                        export SMTP_FROM=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SMTP_FROM')
+                        export SMTP_USER=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SMTP_USER')
+                        export SMTP_PASS=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SMTP_PASS')
 
-                        envsubst < values.yml > values1.yml
-                        echo "showing values1.yml"
+                        # Substitute variables to values file
+                        envsubst < values-${release}.yml > values.yml
+                        echo "showing values.yml"
                         echo "------------------------------------------------------"
-                        cat values1.yml
+                        cat values.yml
                         echo "------------------------------------------------------"
 
                         '''
