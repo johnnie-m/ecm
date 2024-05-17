6e0fd0a27411005f436dd7f67269efb99c833e15
johnnie-m <93076135+johnnie-m@users.noreply.github.com>
2024-05-17 09:35:33 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,24 +3,11 @@
 JSON=$( jq -n \
     --arg password $vltPwd \
     '$ARGS.named')
+
 # Fetch vault user token
 C_TOKEN=$(curl "${vltUrl}/v1/auth/userpass/login/${vltUsr}" --data "$JSON" 2</dev/null | jq -r '.auth.client_token')
+
 # Fetch secrets and export to environment variables
-
-#export ELMA365_AMQP_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_AMQP_URL')
-#export ELMA365_MONGO_DB_NAME=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_MONGO_DB_NAME')
-#export ELMA365_MONGO_PASSWORD=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_MONGO_PASSWORD')
-#export ELMA365_MONGO_USER=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_MONGO_USER')
-#export ELMA365_POSTGRES_DB_NAME=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_POSTGRES_DB_NAME')
-#export ELMA365_POSTGRES_PASSWORD=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_POSTGRES_PASSWORD')
-#export ELMA365_POSTGRES_USER=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_POSTGRES_USER')
-#export ELMA365_RABBITMQ_HOST=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_RABBITMQ_HOST')
-#export ELMA365_RABBITMQ_PASSWORD=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_RABBITMQ_PASSWORD')
-#export ELMA365_RABBITMQ_USER=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_RABBITMQ_USER')
-#export ELMA365_RABBITMQ_VH_NAME=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_RABBITMQ_VH_NAME')
-#export PSQL_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.PSQL_URL')
-#export S3_UPLOAD_METHOD=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.S3_UPLOAD_METHOD')
-
 export HOSTNAME=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.HOSTNAME')
 export IMG_REP=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.IMG_REP')
 export SU_EMAIL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SU_EMAIL')
@@ -42,6 +29,20 @@ export SMTP_PORT=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpu
 export SMTP_FROM=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SMTP_FROM')
 export SMTP_USER=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SMTP_USER')
 export SMTP_PASS=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.SMTP_PASS')
+#export ELMA365_AMQP_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_AMQP_URL')
+#export ELMA365_MONGO_DB_NAME=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_MONGO_DB_NAME')
+#export ELMA365_MONGO_PASSWORD=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_MONGO_PASSWORD')
+#export ELMA365_MONGO_USER=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_MONGO_USER')
+#export ELMA365_POSTGRES_DB_NAME=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_POSTGRES_DB_NAME')
+#export ELMA365_POSTGRES_PASSWORD=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_POSTGRES_PASSWORD')
+#export ELMA365_POSTGRES_USER=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_POSTGRES_USER')
+#export ELMA365_RABBITMQ_HOST=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_RABBITMQ_HOST')
+#export ELMA365_RABBITMQ_PASSWORD=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_RABBITMQ_PASSWORD')
+#export ELMA365_RABBITMQ_USER=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_RABBITMQ_USER')
+#export ELMA365_RABBITMQ_VH_NAME=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.ELMA365_RABBITMQ_VH_NAME')
+#export PSQL_URL=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.PSQL_URL')
+#export S3_UPLOAD_METHOD=$(curl -H "X-Vault-Token: ${C_TOKEN}" -X GET ${vltUrl}/v1/medpult/data/elma365 2</dev/null | jq -r '.data.data.S3_UPLOAD_METHOD')
+
 # Substitute variables to values file
 envsubst < values.yml > values-temp.yml
 echo "showing values.yml"
